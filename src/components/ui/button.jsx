19eac7635012e9f8d772e860cfda2e88ef56f45e
roeeyaish-oss <roeeyaish@gmail.com>
2026-04-01 12:56:13 +0300

    Fix all lint errors

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -3,7 +3,7 @@ import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cn } from "../../lib/utils"
 
-export function Button({ className, variant, asChild = false, ...props }) {
+export function Button({ className, asChild = false, ...props }) {
   const Comp = asChild ? Slot : "button"
   return (
     <Comp
